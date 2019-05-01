00ea88dac489f17f8b32ef00ed8f513bf822f1b0
kawaiiCirno <wwang4292@gmail.com>
2019-05-01 01:23:50 -0700

    Factorial trailing zeroes

---
+++ FactorialTrailingZeroes/FactorialTrailingZeroes.java
@@ -0,0 +1,10 @@
+class FactorialTrailingZeroes {
+    public int trailingZeroes(int n) {
+        int result = 0;
+        while (n > 0) {
+            n /= 5;
+            result += n;
+        }
+        return result;
+    }
+}
