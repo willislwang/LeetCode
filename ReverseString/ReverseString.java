92d72a92f5450f7312fc92da3e24dd040e275e04
kawaiiCirno <wwang4292@gmail.com>
2019-05-17 21:39:54 -0700

    reverse string

---
+++ ReverseString/ReverseString.java
@@ -0,0 +1,9 @@
+class ReverseString {
+    public void reverseString(char[] s) {
+        for (int i = 0; i < s.length / 2; i++) {
+            s[i] = (char)(s[i] + s[s.length - i - 1]);
+            s[s.length - i - 1] = (char)(s[i] - s[s.length - i - 1]);
+            s[i] = (char)(s[i] - s[s.length - i - 1]);
+        }
+    }
+}
