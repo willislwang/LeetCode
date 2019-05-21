728b39b2b2469ae4e4798b895afd279b5ac80495
kawaiiCirno <wwang4292@gmail.com>
2019-05-21 01:33:22 -0700

    reverse link list

---
+++ ReverseLinkList/ReverseLinkList.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class ReverseLinkList {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+        
+    }
+}
